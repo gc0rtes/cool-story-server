8e2cf381db9b2b1d668b4a857cbf51342cff5673
gc0rtes <guilhermeco@gmail.com>
2021-05-31 13:42:12 +0200

    Create models and migration files to space table. Table association between user-space-story

---
+++ models/space.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class space extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      space.belongsTo(models.user);
+      space.hasMany(models.story);
+    }
+  }
+  space.init(
+    {
+      title: { type: DataTypes.STRING, allowNull: false },
+      description: DataTypes.TEXT,
+      backgroundColor: DataTypes.STRING,
+      color: DataTypes.STRING,
+      userId: { type: DataTypes.INTEGER, allowNull: false },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    },
+    {
+      sequelize,
+      modelName: "space",
+    }
+  );
+  return space;
+};
